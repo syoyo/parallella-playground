7311444daf10b4a45af79f41e20c670478e18266
Syoyo Fujita <syoyo@lighttransport.com>
2015-06-13 04:34:00 +0000

    Add linux style file for clang-format.

--- math_exp/host.c
+++ math_exp/host.c
@@ -38,87 +38,93 @@ along with this program, see the file COPYING. If not, see
 #define WAIT_MICROSECONDS (100000)
 #endif
 
-int main(int argc, char *argv[]) {
-  unsigned row, col, coreid, i, j, m, n, k, flag;
-  unsigned int mask0, mask1;
-  e_platform_t platform;
-  e_epiphany_t dev;
-  e_mem_t emem;
-  char emsg[_BufSize];
+int main(int argc, char *argv[])
+{
+	unsigned row, col, coreid, i, j, m, n, k, flag;
+	unsigned int mask0, mask1;
+	e_platform_t platform;
+	e_epiphany_t dev;
+	e_mem_t emem;
+	char emsg[_BufSize];
 
-  unsigned int result[4];
+	unsigned int result[4];
 
-  srand(1);
+	srand(1);
 
-  // initialize system, read platform params from
-  // default HDF. Then, reset the platform and
-  // get the actual system parameters.
-  e_init(NULL);
-  e_reset_system();
-  e_get_platform_info(&platform);
+	// initialize system, read platform params from
+	// default HDF. Then, reset the platform and
+	// get the actual system parameters.
+	e_init(NULL);
+	e_reset_system();
+	e_get_platform_info(&platform);
 
-  // Allocate a buffer in shared external memory
-  // for message passing from eCore to host.
-  e_alloc(&emem, _BufOffset, _BufSize);
+	// Allocate a buffer in shared external memory
+	// for message passing from eCore to host.
+	e_alloc(&emem, _BufOffset, _BufSize);
 
-  // Open a workgroup
-  e_open(&dev, 0, 0, platform.rows, platform.cols);
+	// Open a workgroup
+	e_open(&dev, 0, 0, platform.rows, platform.cols);
 
-  // Load the device program onto all the eCores
-  // To get the verified values
-  // e_load_group("e_math_test.srec", &dev, 0, 0, platform.rows,  platform.cols,
-  // E_FALSE);
+	// Load the device program onto all the eCores
+	// To get the verified values
+	// e_load_group("e_math_test.srec", &dev, 0, 0, platform.rows,
+	// platform.cols,
+	// E_FALSE);
 
-  // To test
-  e_load_group("e_fast_exp_test.srec", &dev, 0, 0, platform.rows,
-               platform.cols, E_FALSE);
+	// To test
+	e_load_group("e_fast_exp_test.srec", &dev, 0, 0, platform.rows,
+		     platform.cols, E_FALSE);
 
-  for (i = 0; i < platform.rows; i++) {
-    for (j = 0; j < platform.cols; j++) {
+	for (i = 0; i < platform.rows; i++) {
+		for (j = 0; j < platform.cols; j++) {
 
-      // Draw to a certain core
-      row = i;
-      col = j;
-      coreid = (row + platform.row) * 64 + col + platform.col;
-      // fprintf(stderr,"%3d: Message from eCore 0x%03x (%2d,%2d) :
-      // \n",(i*platform.cols+j),coreid,row,col);
-      e_start(&dev, i, j);
-      usleep(WAIT_MICROSECONDS); // Wait enough time.
+			// Draw to a certain core
+			row = i;
+			col = j;
+			coreid = (row + platform.row) * 64 + col + platform.col;
+			// fprintf(stderr,"%3d: Message from eCore 0x%03x
+			// (%2d,%2d) :
+			// \n",(i*platform.cols+j),coreid,row,col);
+			e_start(&dev, i, j);
+			usleep(WAIT_MICROSECONDS); // Wait enough time.
 
-      // Wait for core program execution to finish
-      // Read message from shared buffer
+			// Wait for core program execution to finish
+			// Read message from shared buffer
 
-      e_read(&emem, 0, 0, 0x0, emsg, _BufSize);
-      e_read(&dev, i, j, 0x6000, result, sizeof(result));
+			e_read(&emem, 0, 0, 0x0, emsg, _BufSize);
+			e_read(&dev, i, j, 0x6000, result, sizeof(result));
 
-      // Print the message and close the workgroup.
-      // if(flag == 1)
-      //{
-      //}else
-      //{
-      //  fprintf(stderr,"FAIL!\n");
-      //}
-      // Only print out messages on core 0
-      if (i == 0 & j == 0) {
-        fprintf(stderr, "%s\n", emsg);
-        if (result[0] != 1) {
-          fprintf(stderr, "??? TEST FAILED!\n");
-        } else {
-          fprintf(stderr, "[exp] Relative diff: ave = %f, min = %f, max = %f\n",
-                  *(float *)&result[1], *(float *)&result[2],
-                  *(float *)&result[3]);
-        }
-      }
-    }
-  }
+			// Print the message and close the workgroup.
+			// if(flag == 1)
+			//{
+			//}else
+			//{
+			//  fprintf(stderr,"FAIL!\n");
+			//}
+			// Only print out messages on core 0
+			if (i == 0 & j == 0) {
+				fprintf(stderr, "%s\n", emsg);
+				if (result[0] != 1) {
+					fprintf(stderr, "??? TEST FAILED!\n");
+				} else {
+					fprintf(stderr, "[exp] Relative diff: "
+							"ave = %f, min = %f, "
+							"max = %f\n",
+						*(float *)&result[1],
+						*(float *)&result[2],
+						*(float *)&result[3]);
+				}
+			}
+		}
+	}
 
-  // Close the workgroup
-  e_close(&dev);
+	// Close the workgroup
+	e_close(&dev);
 
-  // Release the allocated buffer and finalize the
-  // e-platform connection.
-  e_free(&emem);
-  e_finalize();
+	// Release the allocated buffer and finalize the
+	// e-platform connection.
+	e_free(&emem);
+	e_finalize();
 
-  return 0;
+	return 0;
 }
